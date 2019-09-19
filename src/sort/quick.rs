cf5d2b40a8fe034f4078499a5e0785cf3b28c4a5
FoseFx <fosefx@pm.me>
2019-09-19 12:04:48 +0200

    Fixes QuickSort

--- src/sort/quick.rs
+++ src/sort/quick.rs
@@ -21,12 +21,12 @@ pub fn quick_sort<T: std::cmp::Ord + std::clone::Clone>(vec: &mut Vec<T>) -> &Ve
 /// They are indexes.
 /// 
 fn sort<T: std::cmp::Ord + std::clone::Clone>(vec: &mut Vec<T>, left: usize, right: usize) {
-    if left < right {
-        let frame_we_look_at = handle_frame(vec, left, right);
-        if frame_we_look_at != 0 { // we dont want the 0 - 1 to cause any trouble
-            sort(vec, left, frame_we_look_at - 1);
-        }
-        sort(vec, frame_we_look_at + 1, right);
+    let new_index: isize = handle_frame(vec, left, right) as isize;
+    if (left as isize) < new_index - 1 {
+        sort(vec, left, (new_index as usize) - 1);
+    }
+    if new_index < right as isize {
+        sort(vec, new_index as usize, right);
     }
 }
 
@@ -35,22 +35,21 @@ fn sort<T: std::cmp::Ord + std::clone::Clone>(vec: &mut Vec<T>, left: usize, rig
 /// comparing the values with the pivot defined by `right`
 fn handle_frame<T: std::cmp::Ord + std::clone::Clone>(vec: &mut Vec<T>, left: usize, right: usize) -> usize {
     let mut i: usize = left;
-    let mut j: usize = right - 1;
-    let piv = vec[right].clone();
-    // at this point we have this: . . . left = i . . . . j right . .
-    while i < j { // at the point where i and j meet, the frame was fuly handled
-        while i < right && vec[i] < piv { // i >= right would mean we already went through everything, we look for sth bigger equal piv
+    let mut j: usize = right;
+    let piv = vec[(left + right) / 2].clone();
+    while i <= j { // at the point where i and j meet, the frame was fuly handled
+        while vec[i] < piv { // i >= right would mean we already went through everything, we look for sth bigger equal piv
             i += 1;
         }
-        while j > left && vec[j] >= piv { // j <= left  would mean we already went through everything, we look for sth smaller piv
+        while vec[j] > piv { // j <= left  would mean we already went through everything, we look for sth smaller piv
             j -= 1;
         }
-        while i < j {
+        if i <= j {
             swap(vec, i, j);
+            i += 1;
+            j -= 1;
         }
     }
-    // now move pivot element
-    swap(vec, i, right);
     return i; // i will be in the middle of the frame
 }
 
@@ -66,10 +65,8 @@ mod test {
     use super::*;
     #[test]
     fn quick_sort_test() {
-        println!("hi");
-        let mut vec = vec![5, 7, 2, 1, 8, 4, 9, 10];
-        println!("hi");
+        let mut vec = vec![5, 7, 2, 7, 1, 8, 4, 9, 10];
         quick_sort(&mut vec);
-        assert_eq!(vec, vec![1, 2, 4, 5, 7, 8, 9, 10]);
+        assert_eq!(vec, vec![1, 2, 4, 5, 7, 7, 8, 9, 10]);
     }
 }
